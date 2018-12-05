266cda4eecdc7bee9c65a2dcffedfa4958501922
ReedEJackson <reedejackson@gmail.com>
2018-12-05 11:33:27 -0500

    Fresh Repo yo!!!

---
+++ BucBoard/BucBoard/BucGlobal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BucBoard
+{
+    public class BucGlobal
+    {
+        public static bool BucLoggedIn { get; set; }
+        public static bool BucOffDashboard { get; set; }
+    }
+}
---
+++ BucBoard/BucBoard/Controllers/BucLoginController.cs
@@ -0,0 +1,98 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using System.Web.Helpers;
+using BucBoard.Models;
+using System.Data;
+
+namespace BucBoard.Controllers
+{
+    public class BucLoginController : Controller
+    {
+        // GET: CustomLogin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BucLogin()
+        {
+            BucGlobal.BucLoggedIn = false;
+            return View();
+        }
+
+        public ActionResult BucRegister()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult BucRegister(User bucUser)
+        {
+            if (ModelState.IsValid)
+            {
+                using (bucboardEntities db = new bucboardEntities())
+                {
+                    db.Users.Add(bucUser);
+                    db.SaveChanges();
+                }
+                ModelState.Clear();
+                ViewBag.Message = bucUser.firstName + " " + bucUser.lastName + " was successfully created!";
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult BucLogin(User bucUser)
+        {
+            BucGlobal.BucLoggedIn = false;
+            using (bucboardEntities db = new bucboardEntities())
+            {
+                var bucUsr = db.Users.Where(u => u.email == bucUser.email && u.password == bucUser.password).FirstOrDefault();
+                if (bucUsr != null)
+                {
+                    Session["userID"] = bucUsr.userID.ToString();
+                    Session["email"] = bucUsr.email.ToString();
+                    return RedirectToAction("BucLoggedIn");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Email or Password is incorrect.");
+                }
+            }
+            return View();
+        }
+
+        public ActionResult BucLoggedIn()
+        {
+            if (Session["userID"] != null)
+            {
+                BucGlobal.BucLoggedIn = true;
+                BucGlobal.BucOffDashboard = false;
+                return RedirectToAction("Dashboard", "Dashboard");
+            }
+            else
+            {
+                return RedirectToAction("BucLogin");
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult BucLogOff()
+        {
+            //AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+            BucGlobal.BucLoggedIn = false;
+            return RedirectToAction("CustomLogin", "CustomLogin");
+        }
+
+        private string HashPassword(string pass)
+        {
+            string salt = Crypto.GenerateSalt();
+
+            string hashedPassword = Crypto.HashPassword(salt + pass);
+            return hashedPassword;
+        }
+    }
+}
